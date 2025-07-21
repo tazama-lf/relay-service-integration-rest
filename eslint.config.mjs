8132e7ea46e4e67b3fad992bc0e432fd868942cd
Len Bekker <len.bekker@sybrin.com>
2025-07-21 11:38:15 +0200

    build(deps): use latest tazama library

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
   eslintConfigPrettier,
-  globalIgnores(['**/coverage/**', '**/build/**','**/dist/**', '**/node_modules/**', '**/__tests__/**', '*.ts']),
+  globalIgnores(['**/coverage/**', '**/build/**', '**/dist/**', '**/node_modules/**', '**/__tests__/**', '*.ts']),
   {
     files: ['**/*.ts'],
     plugins: {
